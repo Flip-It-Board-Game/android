f0416dcea51d9a0709c5d2877b5527516df62629
Mike Romani <mr4227@gmail.com>
2017-11-09 14:55:46 -0500

    feat(grid) set up adjustable table, 1 click, 1 change)

--- Flip-It/components/grid.js
+++ Flip-It/components/grid.js
@@ -12,14 +12,6 @@ import {
 import { Container, Header, Content, Button, Text } from 'native-base'
 import Buttons from './buttons'
 
-const styles = StyleSheet.create({
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  TopBuffer: { height: 300, backgroundColor: 'white', borderWidth: 0 },
-  text: { marginLeft: 5 },
-  row: { height: 50, width: 200 },
-  app: { justifyContent: 'center', alignItems: 'center' }
-})
-
 export default class tableView extends Component {
   constructor(props) {
     super(props)
@@ -27,8 +19,8 @@ export default class tableView extends Component {
   }
   render() {
     this.test = true
-    const rowNum = 4
-    const colNum = 4
+    const rowNum = 5
+    const colNum = 5
     let num = rowNum * colNum
 
     let rowButtons = []
@@ -40,18 +32,20 @@ export default class tableView extends Component {
         rowButtons = []
       }
     }
+    const rowWidth = rowNum * 50
+    const styles = StyleSheet.create({
+      TopBuffer: { height: 300, backgroundColor: 'white', borderWidth: 0 },
+      text: { marginLeft: 5 },
+      row: { height: 50, width: rowWidth },
+      app: { justifyContent: 'center', alignItems: 'center' }
+    })
 
     const blankData = []
     return (
       <View style={styles.app}>
         <Table style={styles.table}>
           <Row style={styles.TopBuffer} data={blankData} />
-          <Rows
-            key={'i'}
-            data={tableData}
-            style={styles.row}
-            textStyle={styles.text}
-          />
+          <Rows data={tableData} style={styles.row} textStyle={styles.text} />
         </Table>
       </View>
     )
