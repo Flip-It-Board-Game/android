ecc6a14b70186c68c6e7f61d1d60decadb964b64
apdjz <14apdjz@gmail.com>
2017-11-12 01:17:16 -0500

    added volume slider

--- Flip-It/scene/GameSettings.js
+++ Flip-It/scene/GameSettings.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {    
+  StyleSheet,
+  Text,
+  View,
+  Slider
+} from 'react-native';
 
-class Home extends Component {
-  render() {
+export default class Profile extends Component {
+  constructor(props) {
+   super(props)
+   this.state = { volume: 1 }
+  } 
+  getVal(val){
+  console.warn(val);
+  }     
+  render() {    
+
     return (
-      <View>
-        <Text>
-          TheGreatestComponentInTheWorld could use native Objective-C,
-          Java, or Swift - the product development process is the same.
+      <View style={styles.container}>
+        <Slider
+         style={{ width: 300 }}
+         step={1}
+         minimumValue={1}
+         maximumValue={100}
+         value={this.state.volume}
+         onValueChange={val => this.setState({ volume: val })}
+         onSlidingComplete={ val => this.getVal(val)}
+        />
+        <Text style={styles.welcome}>
+          {this.state.volume}
+        </Text>            
+        <Text style={styles.instructions}>
+          Slide to change volume
         </Text>
       </View>
     );
   }
 }
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
