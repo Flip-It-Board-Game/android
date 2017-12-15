4ef9e9ca76258c8070265aac3283c9ea30501ab1
Mike Romani <mr4227@gmail.com>
2017-12-15 12:03:48 -0500

    layout of levelSelector altered

--- Flip-It/scene/LevelSelector.js
+++ Flip-It/scene/LevelSelector.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
+import { Button } from 'native-base'
 import store, { setBoard, setDimensions } from '../components/store/store'
 import GameScreen from './GameScreen'
 
@@ -11,7 +12,6 @@ class Menu extends Component {
   render() {
     let styles = StyleSheet.create({
       container: {
-        marginTop: 40,
         justifyContent: 'center'
       }
     })
@@ -20,39 +20,75 @@ class Menu extends Component {
       buttonArr.push(
         <View>
           <Button
-            title={`${i} X ${i}`}
+            transparent
+            light
+            style={{ backgroundColor: 'rgba(0,0,0,0)' }}
             onPress={() => {
               store.dispatch(setBoard({ width: i, height: i }))
               store.dispatch(setDimensions({ width: i, height: i }))
               this.props.navigation.navigate('GameScreen')
             }}
-          />
+          >
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: '900',
+                color: 'black',
+                backgroundColor: 'rgba(0,0,0,0)',
+                borderWidth: 1,
+                paddingLeft: 65,
+                paddingRight: 63,
+                paddingTop: 3,
+                borderColor: 'black',
+                textAlign: 'center'
+              }}
+            >
+              {i}X{i}
+            </Text>
+          </Button>
           <Button
-            title={`${i} X ${i + 1}`}
+            transparent
+            light
             onPress={() => {
               store.dispatch(setBoard({ width: i, height: i + 1 }))
               store.dispatch(setDimensions({ width: i, height: i + 1 }))
               this.props.navigation.navigate('GameScreen')
             }}
-          />
+          >
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: '900',
+                color: 'black',
+                backgroundColor: 'rgba(0,0,0,0)',
+                borderWidth: 1,
+                paddingLeft: 65,
+                paddingRight: 63,
+                paddingTop: 3,
+                borderColor: 'black',
+                textAlign: 'center'
+              }}
+            >
+              {i}X{i + 1}
+            </Text>
+          </Button>
         </View>
       )
     }
     return (
       <View style={styles.container}>
-        <Text
+        <Image
           style={{
-            color: '#1a8cff',
-            textAlign: 'center',
-            marginBottom: 15,
-            marginTop: 15,
-            fontSize: 25
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 380,
+            height: 650
           }}
+          source={require('../images/cheap_diagonal_fabric.png')}
         >
-          Select Layout
-        </Text>
-        {buttonArr.map(button => button)}
-        <Text>{'\n'}</Text>
+          {buttonArr.map(button => button)}
+          <Text>{'\n'}</Text>
+        </Image>
       </View>
     )
   }
