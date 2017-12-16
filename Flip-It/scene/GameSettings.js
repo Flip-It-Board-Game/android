f3f84d390926f35e93ba1d49c94c5809618a723b
apdjz <14apdjz@gmail.com>
2017-12-15 19:21:18 -0500

    merge with master

--- Flip-It/scene/GameSettings.js
+++ Flip-It/scene/GameSettings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, AsyncStorage, Image, Text } from 'react-native';
+import { View, AsyncStorage, Image, Text, StyleSheet } from 'react-native';
 import { Button } from 'native-base';
 const Dimensions = require('Dimensions');
 let { height, width } = Dimensions.get('window');
