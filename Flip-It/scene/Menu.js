4a8c2197b6d50014f810ff8a59660a8c54029fd5
Mike Romani <mr4227@gmail.com>
2017-12-16 12:52:58 -0500

    menu page updated

--- Flip-It/scene/Menu.js
+++ Flip-It/scene/Menu.js
@@ -1,165 +1,91 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
-import { Button } from 'native-base';
+import React, { Component } from 'react'
+import { View, StyleSheet, Image, Text } from 'react-native'
+import { Button } from 'native-base'
 
-const Dimensions = require('Dimensions');
-let { height, width } = Dimensions.get('window');
-let tHeight = height;
-let tWidth = width;
+const Dimensions = require('Dimensions')
+let { height, width } = Dimensions.get('window')
+let tHeight = height
+let tWidth = width
 
 class GameMenu extends Component {
   render() {
+    const styles = StyleSheet.create({
+      image: {
+        flex: 1,
+        position: 'absolute',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: tWidth,
+        height: tHeight
+      },
+      text: {
+        fontSize: 17,
+        fontWeight: '900',
+        color: 'black',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderWidth: 1,
+        paddingLeft: 25,
+        paddingRight: 23,
+        paddingTop: 3,
+        borderColor: 'black',
+        width: 170,
+        textAlign: 'center'
+      }
+    })
     return (
       <View>
         <Image
-          style={{
-            flex: 1,
-            position: 'absolute',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: tWidth,
-            height: tHeight
-          }}
+          style={styles.image}
           source={require('../images/snowbackground.gif')}
         >
-          {/* <Image
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: tWidth,
-              height: tHeight
-            }}
-            source={require('../images/snowbackground.gif')}
-          > */}
-            <View>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('GameScreen')}
-              >
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontWeight: '900',
-                    color: 'black',
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderWidth: 1,
-                    paddingLeft: 25,
-                    paddingRight: 23,
-                    paddingTop: 3,
-                    borderColor: 'black',
-                    width: 170,
-                    textAlign: 'center'
-                  }}
-                >
-                  Start
-                </Text>
-              </Button>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('LevelSelector')}
-              >
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontWeight: '900',
-                    color: 'black',
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderWidth: 1,
-                    paddingLeft: 25,
-                    paddingRight: 23,
-                    paddingTop: 3,
-                    borderColor: 'black',
-                    width: 170,
-                    textAlign: 'center'
-                  }}
-                >
-                  Level Select
-                </Text>
-              </Button>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('GameStats')}
-              >
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontWeight: '900',
-                    color: 'black',
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderWidth: 1,
-                    paddingLeft: 25,
-                    paddingRight: 23,
-                    paddingTop: 3,
-                    borderColor: 'black',
-                    width: 170,
-                    textAlign: 'center'
-                  }}
-                >
-                  Game Stats
-                </Text>
-              </Button>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('GameSettings')}
-              >
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontWeight: '900',
-                    color: 'black',
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderWidth: 1,
-                    paddingLeft: 25,
-                    paddingRight: 23,
-                    paddingTop: 3,
-                    borderColor: 'black',
-                    width: 170,
-                    textAlign: 'center'
-                  }}
-                >
-                  Settings
-                </Text>
-              </Button>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('About')}
-              >
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontWeight: '900',
-                    color: 'black',
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderWidth: 1,
-                    paddingLeft: 25,
-                    paddingRight: 23,
-                    paddingTop: 3,
-                    borderColor: 'black',
-                    width: 170,
-                    textAlign: 'center'
-                  }}
-                >
-                  About
-                </Text>
-              </Button>
-              <Button
-                transparent
-                light
-                onPress={() => this.props.navigation.navigate('Test')}
-                title="AsyncStorage Test"
-              />
-            </View>
-          </Image>
-        {/* </Image> */}
+          <View>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('GameScreen')}
+            >
+              <Text style={styles.text}>Start</Text>
+            </Button>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('LevelSelector')}
+            >
+              <Text style={styles.text}>Level Select</Text>
+            </Button>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('GameStats')}
+            >
+              <Text style={styles.text}>Game Stats</Text>
+            </Button>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('GameSettings')}
+            >
+              <Text style={styles.text}>Settings</Text>
+            </Button>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('About')}
+            >
+              <Text style={styles.text}>About</Text>
+            </Button>
+            <Button
+              transparent
+              light
+              onPress={() => this.props.navigation.navigate('Test')}
+              title="AsyncStorage Test"
+            />
+          </View>
+        </Image>
       </View>
-    );
+    )
   }
 }
 
-export default GameMenu;
+export default GameMenu
